5e7b7276d6d418fe183ec8796756f1b700c3c815
Mars <martinianol@outlook.com>
2021-05-11 23:39:46 -0300

    Agregue git ignore sobre Node Modules

---
+++ app.js
@@ -0,0 +1,23 @@
+//console.log('sigo funcionando');
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve (__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log ('Server up and running on port 3000'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.get('/registro', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
