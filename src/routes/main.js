39d679e74e7958d7a7f75e4a219d96116f7f8494
Mars <martinianol@outlook.com>
2021-05-24 15:53:36 -0300

    Comentarios al codigo

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,7 +1,11 @@
+//Requiero express y ejecuto la propiedad Router()
 const express = require('express');
 const router = express.Router();
+
+//requiero el controlador de main
 const mainController = require ('../controllers/mainController');
 
+//Asigno a cada ruta la propiedad del controlador
 router.get('/', mainController.home);
 router.get('/login', mainController.login);
 router.get('/registro', mainController.register);
