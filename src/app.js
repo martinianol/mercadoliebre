296c7a074a60c68348b4ed52022d07cc32296f05
Mars <martinianol@outlook.com>
2021-05-24 12:13:40 -0300

    Comentarios generales en el codigo

--- src/app.js
+++ src/app.js
@@ -6,15 +6,18 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const publicPath = path.resolve (__dirname, '../public');
 
+// Requiero el modulo con las rutas
 const mainRoutes = require('./routes/main.js');
 
+//Determino la carpeta de los archivos publicos para que puedan ser utilizados por Express
 app.use(express.static(publicPath));
 
 app.listen(port, () => console.log ('Server up and running on port 3000'));
 
+//le digo a Express que el motor de templates es EJS
 app.set('view engine', 'ejs');
-//app.set('vews','./views');
 
+//Determino las rutas de las distintas direcciones a que controlador deben llamar
 app.use('/', mainRoutes);
 app.use('/registro', mainRoutes);
 app.use('/login', mainRoutes);
